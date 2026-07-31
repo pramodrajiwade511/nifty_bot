08511591817a79f66617008167791ca309847e24
pramodrajiwade511 <pramoodraje0511@gmail.com>
2026-07-31 18:24:25 +0530

    Refactor check_signals function for better clarity
    Refactor signal checking logic and improve DataFrame handling.

--- main.py
+++ main.py
@@ -97,15 +97,13 @@ def send_morning_levels():
         send_telegram_msg(msg)
     except Exception as e:
         print(f"Error: {e}")
-
 def check_signals():
     global current_trade
-    
-df = yf.download(tickers='^NSEI', period='5d',    interval='5m', progress=False)
- if isinstance(df.columns, pd.MultiIndex):
- df.columns = df.columns.get_level_values(0)                  if df.empty or len(df) < 20:
-  return
-
+    df = yf.download(tickers='^NSEI', period='5d', interval='5m', progress=False)
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = df.columns.get_level_values(0)
+    if df.empty or len(df) <20
+       return 
         st = ta.supertrend(df['High'], df['Low'], df['Close'], length=7, multiplier=3)
         df['ST'] = st['SUPERT_7_3.0']
         df['ST_DIR'] = st['SUPERTd_7_3.0']
